d77a2a8e9b249264ad13b489c4a4f65329677633
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-01 21:44:15 -0700

    new note functionality bug

--- server.js
+++ server.js
@@ -30,19 +30,12 @@ app.get('*', (req, res) => {
 // Functions
 function createNewNote(body, notesArray) {
     const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
+    if (!Array.isArray(notesArray)) notesArray = [];
+    body.id = notesArray.length;
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notesArray }, null, 2)
+        JSON.stringify(notesArray, null, 2)
     );
     return newNote;
 }
